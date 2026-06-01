f84320efe8a5fbcdb81626e063f1cb0ce2b9ec4b
JacobHailemariam <jacob.hailemariam@ucalgary.ca>
2026-06-01 00:26:23 -0600

    Add: is-palindrome - submission-22

---
+++ Data Structures & Algorithms/is-palindrome/submission-22.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = [c for c in s.lower() if c.isalnum()]
+        reverse = []
+        for i in range(len(s)-1,-1,-1):
+            reverse.append(s[i])
+        if s == reverse:
+            return True
+        else:
+            return False
